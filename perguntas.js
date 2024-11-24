fe3e6d5662c5caaf6c6bcec7d914e6836823b3f2
Thata-Mantovani <00001115876892sp@al.educacao.sp.gov.br>
2024-11-24 10:37:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,108 @@
+criaCartao(
+    'Scorpions',
+    'Quando a Banda foi fundada?',
+    'O grupo Scorpions surgiu na cidade de Hanover, Alemanha, em 1971.'
+)
+
+criaCartao(
+    'Scorpions',
+    'Qual foi a formação inicial da Banda?',
+    'A formação inicial  foi constituída pelos irmãos Michael e Rudolf Schenker (Guitarra), Klaus Meine (Voz), Lothar Heimberg (Baixo) e Wolfgang Dziony (Bateria)'
+)
+
+criaCartao(
+    'Scorpions',
+    'Qual foi o primeiro álbum lançado?',
+    ' O primeiro álbum foi gravado em 1972, e intitulado Lonesome Crow.a'
+)
+
+criaCartao(
+    'Scorpions',
+    'Qual foi o ábum mais vendido?',
+    'O maior sucesso da banda veio em 1984, com o LP Love at First Sting,'
+)
+criaCartao(
+    'Scorpions',
+    'Qual é a musica mais ouvida?',
+    'Wind of Change'
+)
+criaCartao(
+    'Scorpions',
+    'Qual é o estilo musical predominante dos Scorpions?',
+    'Os Scorpions são conhecidos principalmente por seu estilo de hard rock e heavy metal, com elementos de rock clássico.'
+)
+criaCartao(
+    'Scorpions',
+    'Quem foi o guitarrista principal da banda por muitos anos?',
+    'O guitarrista principal da banda por muitos anos foi Matthias Jabs, que se juntou à banda em 1978.'
+)
+criaCartao(
+    'Scorpions',
+    'Quem é o vocalista dos Scorpions?',
+    'O vocalista principal da banda é Klaus Meine, que é conhecido por sua voz distinta e potente.'
+)
+criaCartao(
+    'Scorpions',
+    'Quais influências musicais os Scorpions têm?',
+    'Os Scorpions foram influenciados por bandas de rock como The Beatles, The Rolling Stones, Led Zeppelin e Deep Purple?'
+)
+criaCartao(
+    'Scorpions',
+    'Quantos álbuns de estúdio os Scorpions lançaram até 2024?',
+    'Até 2024, os Scorpions lançaram 19 álbuns de estúdio.'
+)
+criaCartao(
+    'Scorpions',
+    'O que motivou a canção "Wind of Change"?',
+    '"Wind of Change" foi inspirada pela queda do Muro de Berlim e pelos eventos políticos que marcaram o fim da Guerra Fria.'
+)
+criaCartao(
+    'Scorpions',
+    'Os Scorpions já realizaram turnês mundiais?',
+    'Sim, os Scorpions realizaram inúmeras turnês mundiais e continuam sendo uma das bandas mais populares no circuito de shows ao vivo.'
+)
+criaCartao(
+    'Scorpions',
+    'Qual é a formação atual dos Scorpions?',
+    'A formação atual é composta por Klaus Meine (vocal), Rudolf Schenker (guitarra), Matthias Jabs (guitarra), Pawel Maciwoda (baixo) e Mikkey Dee (bateria).'
+)
+criaCartao(
+    'Scorpions',
+    'O que aconteceu com o baterista original, Wolfgang Dziony?',
+    'Wolfgang Dziony deixou os Scorpions nos anos 70, sendo substituído por outros bateristas. Ele faleceu em 2015'
+)
+criaCartao(
+    'Scorpions',
+    'Quais são os principais álbuns lançados pelos Scorpions ao longo da carreira?',
+    'Além de "Love at First Sting" e "Blackout", outros álbuns importantes são "Animal Magnetism" (1980), "Crazy World" (1990) e "Savage Amusement" (1988).'
+)
+criaCartao(
+    'Scorpions',
+    'Scorpions já participou de algum grande evento de rock?',
+    'Sim, os Scorpions participaram de grandes eventos como o Monsters of Rock e o Rock in Rio.'
+)
+criaCartao(
+    'Scorpions',
+    'Qual é o estilo de performance ao vivo dos Scorpions?',
+    'A banda é conhecida por suas performances enérgicas e cheias de paixão, com destaque para os solos de guitarra e a presença de palco de Klaus Meine'
+)
+criaCartao(
+    'Scorpions',
+    'Qual foi o impacto dos Scorpions no rock internacional?',
+    'Os Scorpions são uma das bandas mais influentes do rock, especialmente na Alemanha, e ajudaram a popularizar o heavy metal e o hard rock durante as décadas de 1980 e 1990.'
+)
+criaCartao(
+    'Scorpions',
+    'Quais são os planos atuais dos Scorpions?',
+    'Até 2024, os Scorpions continuam em turnê, promovendo seu mais recente álbum, "Rock Believer", e realizando shows em várias partes do mundo. Eles também mencionaram que estão gravando novas músicas.'
+)
+criaCartao(
+    'Scorpions',
+    'Os Scorpions já passaram por alguma grande mudança de formação?',
+    'Sim, ao longo dos anos, os Scorpions passaram por várias mudanças de formação. A saída mais significativa foi a de Klaus Meine em 1974, quando ele teve que se submeter a uma cirurgia nas cordas vocais.'
+)
+criaCartao(
+    'Scorpions',
+    'Qual é o significado da capa do álbum "Lovedrive" (1979)?',
+    'Ela é uma representação visual provocante do amor e do desejo, que se alinhava com o tema do álbum, abordando os aspectos complexos e intensos dos relacionamentos amorosos.'
+)
